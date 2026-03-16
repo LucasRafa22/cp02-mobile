349c82db5b33fc593ce46fae580df7ea584506ca
LucasRafa22 <lucassolimene22@gmail.com>
2026-03-16 11:38:31 -0300

    feat: inputs

--- App.js
+++ App.js
@@ -1,18 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { SafeAreaView } from 'react-native';
 
 export default function App() {
+  const[nomeCliente, setNomeCliente] = useState('')
+  const[emailCliente, setEmailCliente] = useState('')
+  const[mostrarDados, setMostrarDados] = useState(false)
+
   return (
-    <>
-      <Text style="titulo">Titulo</Text>
+    <SafeAreaView style={styles.container}>
+      <View >
     
-    </>
+        <Text style={styles.title}>Cadastro</Text>
+        <TextInput 
+          placeholder='Digite seu nome'
+          value={nomeCliente}
+          maxLength={30}
+          autoCapitalize='none'
+          onChange={(valor) => setNomeCliente(valor)}
+        />
+        <TextInput 
+          placeholder='Digite seu nome'
+          value={nomeCliente}
+          maxLength={30}
+          autoCapitalize='none'
+          onChange={(valor) => setEmailCliente(valor)}
+        />
+        <Button
+          title='Clique aqui para enviar'
+          color={'#000'}
+          onPress={()=>setMostrarDados(true)}
+        />
+
+        {mostrarDados&&<RenderizarDados nameCliente={nomeCliente} emailCliente={emailCliente}/>}
+    
+      </View>
+    </SafeAreaView>
   );
 }
 
-const titulo = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
-    fontSize: 24,
+    flex:1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  title: {
+    fontSize: 24
+  }
 });
